81e2d0021cd59cd765063e5c3a7817e1ec5676fe
Seth22 <41117493+Seth22@users.noreply.github.com>
2022-10-05 21:05:15 -0400

    -Added comments to explain code

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+//Student loan calculator!
+//Author: Seth/Sheriff John Brown
 import java.util.Scanner;
 import java.util.InputMismatchException;
 import java.text.DecimalFormat;
@@ -12,6 +14,7 @@ public class Main {
     }
     static void setI(double newI) {
         I = (newI/12)/100;
+        //Takes the interest as a percent then divides by 12(period) of the loan payments and divides by 100 to get decimal value
     }
     static double getI() {
         return I;
@@ -19,6 +22,8 @@ public class Main {
     static void setT(double newT) {
         T = newT*12;
         OT = newT;
+        //sets t = to years(term) of debt times the number of payments(monthly/12)
+        //sets ot = to years(term) of the debt
     }
     static double getT() {
         return T;
@@ -38,6 +43,7 @@ public class Main {
             studentInterest();
         }
         catch(InputMismatchException ex) {
+            //If user does not input an integer it will set the debt to 80k
             System.out.println("Setting Student debt Debt to $80,000");
             setPV(80000);
             studentInterest();
@@ -50,6 +56,7 @@ public class Main {
             setI(interestrate.nextDouble());
             studentYears();
         } catch (InputMismatchException ex) {
+            //If user does not input an integer it will set the APY to 4.99%(current federal student debt interest rate)
             System.out.println("Setting Interest rate to 4.99% APY");
             setI(4.99);
             studentYears();
@@ -63,6 +70,7 @@ public class Main {
             printStudent();
         }
         catch(InputMismatchException ex) {
+            //If user does not input an integer it will set the years to 30(Being debt free by ~50 years old)
             System.out.println("Setting debt free time to 30 years");
             setT(30);
             printStudent();
@@ -78,6 +86,7 @@ public class Main {
                 DecimalFormat decimalFormat = new DecimalFormat("#.##");
                 decimalFormat.setGroupingUsed(true);
                 decimalFormat.setGroupingSize(3);
+                //Formats the outputted numbers to have commas and go the second decimal place(money format)
                 for (int x=5;x <= 50;x+=5) {
                     setT(x);
                     System.out.println("When you take "+x+" years "+"Your monthly payments will be: $"+decimalFormat.format(studentPayments()));
@@ -97,6 +106,8 @@ public class Main {
                     setT(x);
                     System.out.println("When you take "+x+" years "+"The total interest paid will be: $"+decimalFormat.format((studentPayments()*12*getOT())-getPV()));
                 }
+                //will calculate monthly/yearly payments,Total interest cost, total cost, total interest cost every 5 years from 5 to 50
+                //Used 50 years as max because if you graduate college at 21 then you will be 71 when you are debt free
                 anotherOne();
             }
             else {
@@ -104,6 +115,8 @@ public class Main {
             }
         }
         catch(InputMismatchException ex) {
+            //If user does not input an integer goes back to ask the user if they want to calculate more loans
+            //Goes back the place the user started
             anotherOne();
         }
     }
@@ -125,11 +138,16 @@ public class Main {
     }
     public static Double studentPayments() {
         return getPV()*((getI()*Math.pow((1+getI()),getT()))/(Math.pow((1+getI()),getT())-1));
+        //The PMT formula - basically modified Present Value Annuity Formula(PVA) A.K.A the formula that determines loan costs
+        // Principle is times by [Interest is times by (interest +1) raised to the power of years, divided by (interest +1) raised to the power of (time-1)]
+        // Your monthly payments will be lowered on a reverse inverse exponential curve based on years taken to pay off
+        // Total cost/interest will be exponential increased based on the time to take off
     }
     public static void printStudent() {
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         decimalFormat.setGroupingUsed(true);
         decimalFormat.setGroupingSize(3);
+        //Formats the outputted numbers to have commas and go the second decimal place(money format)
         System.out.print("Your monthly payments will be: $"+decimalFormat.format(studentPayments()));
         System.out.print("\nYour yearly payments will be: $"+decimalFormat.format(studentPayments()*12));
         System.out.print("\nTotal amount paid will be: $"+decimalFormat.format(studentPayments()*12*getOT()));
